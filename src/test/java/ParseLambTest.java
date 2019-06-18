8be0cd91b43b8edbbc1f06f06a6143a6e19cf11d
avajhubgit <dima893@gmail.com>
2019-06-18 13:25:02 +0300

    Change ParseLambTest.

--- src/test/java/ParseLambTest.java
+++ src/test/java/ParseLambTest.java
@@ -14,7 +14,7 @@ public class ParseLambTest {
     @Test
     public void extractNumberWithLambda() {
         ParseLamb parseLamb = new ParseLamb();
-        assertEquals("", "", parseLamb.extractNumberFromString(""));
+        assertEquals("", "", parseLamb.extractNumberFromcommString(""));
         assertEquals("", "0123456789.", parseLamb.extractNumberFromString("0123456789."));
         assertEquals("", "78025679", parseLamb.extractNumberFromString("78x025K67z9"));
 
